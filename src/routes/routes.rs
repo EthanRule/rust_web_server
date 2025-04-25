65da2c67e159f804196aef7b2e1b1e1d2efd98a1
Ethan Rule <94942533+EthanRule@users.noreply.github.com>
2025-04-24 18:52:50 -0700

    fixed funcs

--- src/routes/routes.rs
+++ src/routes/routes.rs
@@ -1,23 +1,22 @@
 use http_body_util::{BodyExt, Empty, Full, combinators::BoxBody};
 use hyper::body::{Body, Bytes};
-use hyper::{Method, Request, Response, StatusCode};
-
-use crate::routes::methods; 
+use hyper::{Method, Request, Response, StatusCode}; 
 
 pub async fn handle_request(req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     match (req.method()) {
-        &Method::CONNECT => methods::connect(req),
-        &Method::DELETE => methods::delete(req),
-        &Method::GET => methods::get(req),
-        &Method::HEAD => methods::head(req),
-        &Method::OPTIONS => methods::options(req),
-        &Method::PATCH => methods::patch(req),
-        &Method::POST => methods::post(req),
-        &Method::PUT => methods::put(req),
-        &Method::TRACE => methods::trace(req),
+        &Method::CONNECT => connect(req),
+        &Method::DELETE => delete(req),
+        &Method::GET => get(req),
+        &Method::HEAD => head(req),
+        &Method::OPTIONS => options(req),
+        &Method::PATCH => patch(req),
+        &Method::POST => post(req),
+        &Method::PUT => put(req),
+        &Method::TRACE => trace(req),
         _ => Err("Invalid request method."),
     };
 }
+
 
 fn empty() -> BoxBody<Bytes, hyper::Error> {
     Empty::<Bytes>::new().map_err(|never| match never {}).boxed()
